28fa3ee19bd852c6cf8e7e64265e3675f3f3591d
Actek98 <46975083+Actek98@users.noreply.github.com>
2019-02-13 00:04:57 +0200

    List is ready

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -123,9 +123,10 @@ namespace UnitTestProject
         [TestMethod]
         public void IEnumerableTestMethod()
         {
-            MyListArray<int> list = new MyListArray<int>();
+            MyListArray<int> list = new MyListArray<int>(100);
             var e = list.GetEnumerator();
             Assert.IsFalse(e.MoveNext());
+            e.Reset();
             int[] mas = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             foreach (int val in mas)
             {
@@ -135,24 +136,127 @@ namespace UnitTestProject
             using (IEnumerator<int> le = list.GetEnumerator())
             {
                 while (le.MoveNext())
-                {
                     Assert.AreEqual(le.Current, mas[count++]);
-                }
             }
         }
 
         [TestMethod]
         public void CopyToTestMethod()
         {
-            int[] array = new int[5];
+            int[] array = new int[5] { 9, 8, 7, 6, 5 };
             MyListArray<int> list = new MyListArray<int>(3);
             list.Add(100);
             list.Add(101);
             list.Add(102);
             list.CopyTo(array, 2);
+            Assert.AreEqual(9, array[0]);
+            Assert.AreEqual(8, array[1]);
             Assert.AreEqual(list[0], array[2]);
             Assert.AreEqual(list[1], array[3]);
             Assert.AreEqual(list[2], array[4]);
+        }
+
+        [ExpectedException(typeof(ArgumentNullException))]
+        [TestMethod]
+        public void CopyToNullTestMethod()
+        {
+            int[] array = null;
+            MyListArray<int> list = new MyListArray<int>(1);
+            list.Add(100);
+            list.CopyTo(array, 2);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void CopyToOutOfRangeTestMethod()
+        {
+            int[] array = new int[5];
+            MyListArray<int> list = new MyListArray<int>(2);
+            list.Add(100);
+            list.Add(101);
+            list.CopyTo(array, -1);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void CopyToArgExeptionTestMethod()
+        {
+            int[] array = new int[5];
+            MyListArray<int> list = new MyListArray<int>(2);
+            list.Add(100);
+            list.Add(101);
+            list.CopyTo(array, 4);
+        }
+
+        [TestMethod]
+        public void IndexOfTestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);
+            list.Add(100);
+            list.Add(101);
+            Assert.AreEqual(list.IndexOf(100), 0);
+            Assert.AreEqual(list.IndexOf(101), 1);
+            Assert.AreEqual(list.IndexOf(102), -1);
+        }
+
+        [TestMethod]
+        public void InsertTestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);
+            list.Add(100);
+            list.Add(101);
+            list.Insert(2, 7);
+            list.Insert(1, 6);
+            list.Insert(0, 5);
+            Assert.AreEqual(list[0], 5);
+            Assert.AreEqual(list[1], 100);
+            Assert.AreEqual(list[2], 6);
+            Assert.AreEqual(list[3], 101);
+            Assert.AreEqual(list[4], 7);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void InsertOutOfRangeTestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);
+            list.Add(100);
+            list.Add(101);
+            list.Insert(3, 4);
+        }
+
+        [TestMethod]
+        public void RemoveTestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(3);
+            list.Add(100);
+            list.Add(101);
+            list.Add(103);
+            Assert.AreEqual(list.Remove(100), true);
+            Assert.AreEqual(list.Remove(103), true);
+            Assert.AreEqual(list.Remove(102), false);
+        }
+
+        [TestMethod]
+        public void RemoveAtTestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(3);
+            list.Add(100);
+            list.Add(101);
+            list.Add(103);
+            Assert.AreEqual(list.Remove(100), true);
+            Assert.AreEqual(list.Remove(103), true);
+            Assert.AreEqual(list.Remove(102), false);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void RemoveAtOutOfRangeTestMethod()
+        {
+            MyListArray<int> list = new MyListArray<int>(2);
+            list.Add(100);
+            list.Add(101);
+            list.RemoveAt(2);
         }
     }
 }
